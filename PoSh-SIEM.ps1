7dd4ecb97fea93ac8d52cbbefb4e28f3622e7c1f
Daniel Komnick <daniel.s.komnick.mil@army.mil>
2022-04-28 20:33:03 -0400

    initial commit

---
+++ PoSh-SIEM.ps1
@@ -0,0 +1,8 @@
+﻿
+Start-Job -ScriptBlock {
+    & "C:\Users\danie\Documents\GitHub\PoSh-EasyWin\DevOps\PowerSIEM.ps1"
+} -Name "high101bro"
+
+Get-Job -Name "high101bro" | Receive-Job -Wait | Out-GridView -Title "PowerSiem - Powered by high101bro!"
+
+& "C:\Users\danie\Documents\GitHub\PoSh-EasyWin\DevOps\PowerSIEM.ps1"
